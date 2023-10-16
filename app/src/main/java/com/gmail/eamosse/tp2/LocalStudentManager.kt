43995f02ef29e2743151c06f28285d6a65a91467
tianyiLE <t.le@net.estia.fr>
2023-10-16 19:54:41 +0200

    16/10/2023 19：51

--- app/src/main/java/com/gmail/eamosse/tp2/LocalStudentManager.kt
+++ app/src/main/java/com/gmail/eamosse/tp2/LocalStudentManager.kt
@@ -60,18 +60,5 @@ class LocalStudentManager : StudentManager {
     override fun clear() {
         students.clear()
     }
-    /**
-     * listOf(
-    Student(name = "John1", code = "A1", sexe = "M", address = "Lilles", age = 20),
-    Student(name = "John2", code = "A2", sexe = "F", address = "Lilles", age = 30),
-    Student(name = "John3", code = "A3", sexe = "F", address = "Lilles", age = 41),
-    Student(name = "John4", code = "A4", sexe = "M", address = "Lilles", age = 42),
-    Student(name = "John5", code = "A5", sexe = "M", address = "Lilles", age = 34),
-    Student(name = "John6", code = "A6", sexe = "F", address = "Lilles", age = 12),
-    Student(name = "John7", code = "A7", sexe = "M", address = "Lilles", age = 28),
-    Student(name = "John8", code = "A8", sexe = "M", address = "Lilles", age = 16),
-    Student(name = "John9", code = "A9", sexe = "F", address = "Lilles", age = 10),
-    Student(name = "John10", code = "A10", sexe = "F", address = "Lilles", age = 56),
-    )
-     */
+
 }
